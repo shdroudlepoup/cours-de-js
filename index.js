05356b97fb03f1e93b3ea68d896228d347473c0b
SHDROUDLEPOUP <sir.remi.guimard@gmail.com>
2024-04-02 19:02:21 +0400

    Premier travail Remi

--- index.js
+++ index.js
@@ -183,9 +183,6 @@ app.post('/moteur', async (req, res) => {
   }
 })
 
-console.log('Listen on port 3000');
-app.listen(3000);
-
 /*
   TODO:
 
@@ -193,3 +190,118 @@ app.listen(3000);
   * faire une autre URL /voitures qui fait la même chose que /moteur mais sur la table "gammeporsche"
   * exercice suivant (qu'on fera ensemble), le champ "moteur" de la page /voitures sera un <input type="select"> utilisant les moteurs de la table "moteurporsche"
 */
+
+const displayGammeporsche = async (req, res) => {
+  console.log('Requete display');
+  const result = await clientDB.query('select * from gammeporsche order by modele;');
+  let tableRows = '';
+  result.rows.forEach(row => {
+    tableRows += `<form method="POST">
+      <input type="hidden" name="update" value="1" />
+      <input type="hidden" name="ancienmodele" value="${row.modele}" /></td>
+      <tr>
+        <td><input name="modele" value="${row.modele}" /></td>
+        <td><input name="moteur" value="${row.moteur}" /></td>
+        <td><input name="puissance" value="${row.puissance}" /></td>
+        <td><button type="submit">Sauvegarde ligne</button></td>
+        <td><button onclick="efface('${row.modele}','${row.moteur}','${row.puissance}')">Boom</button></td>
+      </tr>
+    </form>
+    `;
+  });
+
+  res.send(`<html>
+  <body>
+      Hello <b>toto</b>
+      <table>
+        <tr>
+          <th>Modele</th>
+          <th>Moteur</th>
+          <th>Puissance</th>
+        </tr>
+        ${tableRows}
+      </table>
+      <h1>Ajouter un modele</h1>
+      <form method="POST">
+        <label>Modele<input name="modele" /></label>
+        <label>Moteur<input name="moteur" /></label>
+        <label>Puissance<input name="puissance" /></label>
+        <button type="submit">OK</button>
+      </form>
+  </body>
+  <script>
+  function efface(modele, moteur, puissance) {
+    console.log('Efface'),
+    fetch('/gammeporsche', {
+      method: 'post',
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body:  JSON.stringify({
+        delete: 1,
+        modele,
+        moteur,
+        puissance,
+      })
+    }).then((resultat) => {
+      console.log('end of fetch');
+      location.reload();
+    })
+  }
+  </script>
+  </html>`)
+};
+
+app.get('/gammeporsche', async (req, res) => {
+  await displayGammeporsche(req,res)
+})
+
+app.post('/gammeporsche', async (req, res) => {
+  try {
+    if (req.body.delete) {
+      console.log('Requete DELETE');
+      await clientDB.query(
+        {
+            text: 'DELETE FROM gammeporsche WHERE modele=$1 AND moteur=$2 AND puissance=$3',
+            values: [req.body.modele, req.body.moteur, req.body.puissance]
+        }
+      )
+    }
+    else if (req.body.update) {
+      console.log('Requete update', req.body);
+      await clientDB.query(
+        {
+          text: 'UPDATE gammeporsche SET modele=$1, moteur=$2, puissance=$3 WHERE modele=$4',
+          values: [req.body.modele, req.body.moteur, req.body.puissance, req.body.ancienmodele]
+        }
+      )
+    }
+    else if ( req.body.modele && req.body.moteur && req.body.puissance) {
+      console.log('Requete AJOUTE');
+      await clientDB.query(
+          {
+              text: 'INSERT INTO gammeporsche (modele, moteur, puissance) VALUES($1, $2, $3)',
+              values: [req.body.modele, req.body.moteur, req.body.puissance]
+          }
+      )
+    }
+    else {
+      console.log('RIEN', req.body)
+    }
+    res.redirect('/gammeporsche');
+  } catch(error) {
+    console.error('Erreur', error)
+    res.send(`
+      <html>
+        <body>
+          Une erreur est survenue.<br/>
+          <a href="/gammeporsche">Continuer</a>
+        </body>
+      </html>
+    `);
+  }
+})
+
+console.log('Listen on port 3000');
+app.listen(3000);
